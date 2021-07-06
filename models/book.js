28f2e606e1150b1e49030fe83fe337de1ca1bfdf
Asia Alnahi <asia.alius@gmail.com>
2021-07-06 14:59:36 -0700

    create book model

--- models/book.js
+++ models/book.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+  _id: { type: String },
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  authors: { type: Array, required: true },
+  description: { type: String },
+  image: { type: String },
+  link: { type: String }
 });
 
 const Book = mongoose.model("Book", bookSchema);
