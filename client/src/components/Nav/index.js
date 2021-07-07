4d025169e13ccd7a94a59c41c7f8e74752cd23fa
Asia Alnahi <asia.alius@gmail.com>
2021-07-06 21:05:25 -0700

    change the nave bar color

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './style.css';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Google Books Search
       </a>
     </nav>
   );
