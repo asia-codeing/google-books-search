d364f50a6f927f3bed3bdddb3bbf6de4b8cb17ba
Asia Alnahi <asia.alius@gmail.com>
2021-07-13 14:21:55 -0700

    Fixing the models

--- models/book.js
+++ models/book.js
@@ -2,12 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  _id: { type: String },
-  title: { type: String, required: true },
-  authors: { type: Array, required: true },
-  description: { type: String },
-  image: { type: String },
-  link: { type: String }
+  
+  title: { 
+    type: String, 
+    required: true 
+  },
+  authors: { 
+    type: Array, 
+    required: true 
+  },
+  description: { 
+    type: String 
+  },
+  image: { 
+    type: String 
+  },
+  link: { 
+    type: String 
+  }
 });
 
 const Book = mongoose.model("Book", bookSchema);
