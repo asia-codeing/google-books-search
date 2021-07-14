cd55b24ac19b18340aa0030f4d1f9e76e9c46fc0
Asia Alnahi <asia.alius@gmail.com>
2021-07-13 17:42:46 -0700

    Edit the App.jsx file and remove unnessesary lines

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,46 +21,8 @@ function App() {
         </Switch>
       </>
     </Router>
-
-    // <div className="App">
-    //   <div className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <h2>Welcome to React</h2>
-    //   </div>
-    //   <p className="App-intro">
-    //     To get started, edit <code>src/App.js</code> and save to reload.
-    //   </p>
-    // </div>
   );
 }
 
 
 export default App;
-
-
-// import React from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Books from "./pages/Books";
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
-// import Header from './components/Header/Header';
-// import BookBox from './components/BookBox/BookBox';
-// import SavedBooks from './pages/SavedBooks';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Header/>
-//           <Switch>
-//             <Route exact path="/" component={Books}>
-//             </Route>
-//             <Route exact path="/savedbooks" component={SavedBooks}>
-//             </Route>
-//           </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
